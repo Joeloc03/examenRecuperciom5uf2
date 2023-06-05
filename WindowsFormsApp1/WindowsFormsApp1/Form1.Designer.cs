ac20872a2a102a115525dffbaa307dae2591ad6d
Joel Oliveras <joeloliveras03@gmail.com>
2023-06-05 12:11:21 +0200

    modificacio del projecte i creacio de botons

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btMenjar = new System.Windows.Forms.Button();
+            this.btViatje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btMenjar
+            // 
+            this.btMenjar.Location = new System.Drawing.Point(203, 224);
+            this.btMenjar.Name = "btMenjar";
+            this.btMenjar.Size = new System.Drawing.Size(133, 50);
+            this.btMenjar.TabIndex = 0;
+            this.btMenjar.Text = "menjar";
+            this.btMenjar.UseVisualStyleBackColor = true;
+            // 
+            // btViatje
+            // 
+            this.btViatje.Location = new System.Drawing.Point(447, 224);
+            this.btViatje.Name = "btViatje";
+            this.btViatje.Size = new System.Drawing.Size(152, 50);
+            this.btViatje.TabIndex = 1;
+            this.btViatje.Text = "viatge";
+            this.btViatje.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btViatje);
+            this.Controls.Add(this.btMenjar);
+            this.Name = "FrmMain";
+            this.Text = "FrmMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btMenjar;
+        private System.Windows.Forms.Button btViatje;
     }
 }
 
